6451e2e246d52fd3d21eb8bc7f790a2207f8109a
sabapen <sabaochipenguin@gmail.com>
2024-01-08 22:16:46 +0900

    add:検索時ID表示

--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -1,9 +1,11 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import joyokanjis from '../references/joyokanji.json'
+import { get } from 'http'
 
 const Search = () => {
   const [searchedYojijukugo, setSearchedYojijukugo] = useState('')
+  const [searchedYojijukugoId, setSearchedYojijukugoId] = useState(0)
 
   const handleSearch = (event, formData) => {
     event.preventDefault()
@@ -12,27 +14,30 @@ const Search = () => {
       return;
     }
     setSearchedYojijukugo(formData.yojijukugo)
+    setSearchedYojijukugoId(getYojijukugoId(formData.yojijukugo));
   }
 
   return (
     <>
       <h1>完全一致検索</h1>
-      <h2>検索したい四字熟語を四文字の漢字で入力してください。（常用漢字のみ対応しています。）</h2>
+      <h2>
+        検索したい四字熟語を四文字の漢字で入力してください。（常用漢字のみ対応しています。）
+      </h2>
       <div>
         <YojijukugoForm handleSearch={handleSearch} />
       </div>
+      {/* 検索ボタンが押されたら結果を表示する */}
+      {searchedYojijukugo && (
+        <div>
+          <p>ID: {searchedYojijukugoId}</p>
+          <p>四字熟語: {searchedYojijukugo}</p>
+        </div>
+      )}
       <div>
         <p>
           <Link to='/all-yojijukugo-DB/'>トップに戻る</Link>
         </p>
       </div>
-      {/* 検索ボタンが押されたら結果を表示する */}
-      {searchedYojijukugo && (
-        <div>
-          <p>ID: {0}</p>
-          <p>四字熟語: {searchedYojijukugo}</p>
-        </div>
-      )}
     </>
   )
 }
@@ -69,12 +74,12 @@ function YojijukugoForm({ handleSearch }) {
 }
 
 interface joyokanji {
-  id: number,
+  kanjiId: number,
   kanji: string,
 }
 
 const joyokanjiArray: joyokanji[] = Object.keys(joyokanjis).map((key: string) => ({
-  id: parseInt(key),
+  kanjiId: parseInt(key),
   kanji: joyokanjis[key],
 }))
 
@@ -91,4 +96,16 @@ function AssertYojijukugo(yojijukugo: string): boolean {
     }
   }
   return true;
+}
+
+function getYojijukugoId(yojijukugo: string): number {
+  const str = [...yojijukugo]
+  const kanjiIds = str.map((kanji) => joyokanjiArray.find((joyokanji) => joyokanji.kanji === kanji)!.kanjiId)
+  const yojijukugoId =
+    (kanjiIds[0] - 1) * joyokanjiArray.length ** 3 +
+    (kanjiIds[1] - 1) * joyokanjiArray.length ** 2 +
+    (kanjiIds[2] - 1) * joyokanjiArray.length +
+    (kanjiIds[3] - 1)
+    + 1;
+  return yojijukugoId;
 }
