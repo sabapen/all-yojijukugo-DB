95bc6ae479bfdab5a234f240382b9a034bb0677a
sabapen <sabaochipenguin@gmail.com>
2024-01-08 20:09:56 +0900

    fix:import React

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import './App.css'
 import { BrowserRouter } from 'react-router-dom'
 import { Router } from './Router'
-import React from 'react'
 
 function App() {
   return (
--- src/Router.tsx
+++ src/Router.tsx
@@ -1,6 +1,5 @@
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/home'
-import React from 'react'
 
 export const Router = () => {
   return (
